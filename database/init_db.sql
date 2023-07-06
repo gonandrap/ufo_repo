6f3b526edcb9da3a6b5c9054c534839d44564447
Gonzalo <gonzalo.a.r@gmail.com>
2023-07-06 12:13:44 -0700

    refactor of the code to better adjust directory structure to use with docker

---
+++ database/init_db.sql
@@ -0,0 +1,24 @@
+-- Active: 1687640843759@@127.0.0.1@5432@ufo
+create table if not EXISTS ufo_description (
+    obs_id VARCHAR(32) not null,
+    obs_ocurred TIME,
+    obs_reported TIME,
+    obs_summary VARCHAR(300),
+    obs_detailed_description varchar(1000),
+    PRIMARY KEY (obs_id),
+    FOREIGN KEY (obs_id) REFERENCES ufo_observation
+);
+
+CREATE TYPE shape AS ENUM if not EXISTS ('Oval', 'Triangle', 'Light', 'Dark', 'Orb', 'Fireball', 'Circle', 'Changing', 'Disk', 'Cylinder', 'Rectangle', 'Unknown', 'Changing', 'Egg', 'cube', 'Sphere', 'Formation', 'Chevron', 'Flash', 'Disk', 'Star', 'Other', 'Cone');
+
+create table if not EXISTS ufo_observation (
+    obs_id VARCHAR(32) not null,
+    obs_posted DATE,
+    obs_city VARCHAR(100),
+    obs_state VARCHAR(100),
+    obs_country VARCHAR(100),
+    obs_shape VARCHAR(50),
+    obs_duration VARCHAR(50),
+    obs_images BOOLEAN default false,
+    PRIMARY KEY (obs_id)
+);
