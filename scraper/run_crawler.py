75f4abea327d93f3fee722c197fe019c8f82ddbb
Gonzalo <gonzalo.a.r@gmail.com>
2023-07-07 18:07:56 -0700

    crontab working in container

--- scraper/run_crawler.py
+++ scraper/run_crawler.py
@@ -1,3 +1,5 @@
+#!/usr/local/bin/python3
+
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scraper_project.spiders.observations import ObservationsSpider
@@ -5,15 +7,20 @@ from datetime import datetime
 import os 
 import logging
 
-date_pattern = '%m/%d/%Y'
 
-date_from = datetime.strftime(datetime.now(), date_pattern)
-if 'DATE_FROM' in os.environ:
-    try:
-        date_from = os.environ['DATE_FROM']
-    except ValueError as e:
-        logging.warning(f"`Env variable DATE_FROM=[{os.environ['DATE_FROM']}] doesn't match [{date_pattern}]. Using today's date instead")
+def main():
+    DATE_PATTERN = '%m/%d/%Y'
 
-process = CrawlerProcess(get_project_settings())
-process.crawl(ObservationsSpider, url='https://nuforc.org/webreports/ndxevent.html', date_from=date_from, bucket_name='codingchallengeimportfiles', table_name='scrap_run')
-process.start()
+    date_from = datetime.strftime(datetime.now(), DATE_PATTERN)
+    if 'DATE_FROM' in os.environ:
+        try:
+            date_from = os.environ['DATE_FROM']
+        except ValueError as e:
+            logging.warning(f"`Env variable DATE_FROM=[{os.environ['DATE_FROM']}] doesn't match [{DATE_PATTERN}]. Using today's date instead")
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(ObservationsSpider, url='https://nuforc.org/webreports/ndxevent.html', date_from=date_from, bucket_name='codingchallengeimportfiles', table_name='scrap_run')
+    process.start()
+
+if __name__ == '__main__':
+    main()
