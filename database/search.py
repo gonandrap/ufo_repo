9e7951b49aede4a464cef5049a7362e5448c3001
gonandrap <gonzalo.a.r@gmail.com>
2023-07-12 10:33:47 -0700

    finished testing db search layer

--- database/search.py
+++ database/search.py
@@ -1,6 +1,6 @@
 import logging
 from connection import UFODatabase
-from datetime import date
+from datetime import datetime, date, timedelta
 from sqlalchemy import select
 
 class DBSearch:
@@ -13,22 +13,19 @@ class DBSearch:
         self.logger = logging.getLogger('db_search')
 
     def search_by_date(self, date : date) -> list:
-        ufo_obs_table = self.db.get_table('ufo_observation')
-        ufo_desc_table = self.db.get_table('ufo_description')
-        stmt = select(ufo_obs_table, ufo_desc_table).where(
-                ufo_obs_table.c.obs_posted == date,                             # the correct columns to use are ufo_desc_table.c.obs_ocurred or ufo_desc_table.c.obs_reported, but only considerig the date, not the time.
-                ufo_obs_table.c.obs_id == ufo_desc_table.c.obs_id
-                )
-        return self.__execute_query(stmt)
+        """
+            Returns all observations that ocurred during the day represented by **date** (ignoring matching exact time)
+        """
+        return self.search_by_date_range(*self.__to_date_range(date))
         
     def search_by_date_range(self, date_from : date, date_to : date) -> list:
         ufo_obs_table = self.db.get_table('ufo_observation')
         ufo_desc_table = self.db.get_table('ufo_description')
         stmt = select(ufo_obs_table, ufo_desc_table).where(
-                ufo_obs_table.c.obs_posted >= from_date,                        # the correct columns to use are ufo_desc_table.c.obs_ocurred or ufo_desc_table.c.obs_reported, but only considerig the date, not the time.
-                ufo_obs_table.c.obs_posted <= to_date,
+                ufo_desc_table.c.obs_ocurred >= date_from,
+                ufo_desc_table.c.obs_ocurred <= date_to,
                 ufo_obs_table.c.obs_id == ufo_desc_table.c.obs_id
-                )
+                ).order_by(ufo_desc_table.c.obs_ocurred)
         return self.__execute_query(stmt)
 
 
@@ -38,7 +35,7 @@ class DBSearch:
         stmt = select(ufo_obs_table, ufo_desc_table).where(
                 ufo_obs_table.c.obs_city == location,
                 ufo_obs_table.c.obs_id == ufo_desc_table.c.obs_id
-                )
+                ).order_by(ufo_desc_table.c.obs_ocurred)
         return self.__execute_query(stmt)
 
     def search_all(self):
@@ -59,6 +56,15 @@ class DBSearch:
             self.logger.error(f'Error trying to execute query [{stmt}] -> {repr(e)}')
             return []
 
+    def __to_date_range(self, specific_datetime : datetime) -> (datetime, datetime):
+        """
+            Given a datetime, it will return two datetimes that represent the beginning and end of the 
+            date for that particular datetime 
+        """
+        from_datetime = datetime.strptime(datetime.strftime(specific_datetime, "%m/%d/%Y"), "%m/%d/%Y")
+        to_datetime = from_datetime + timedelta(days=1)
+        return (from_datetime, to_datetime)
+
 
 # ----- TESTING ----- 
 def set_env_variables():
@@ -74,13 +80,23 @@ def print_results(results):
     for row in results:
         print(row)
 
+def test_search_by_date_range():
+    from datetime import datetime
+
+    set_env_variables()
+    db_search = DBSearch()
+
+    date_from = datetime.strptime('07/01/2023', '%m/%d/%Y')
+    date_to = datetime.strptime('07/31/2023', '%m/%d/%Y')
+    print_results(db_search.search_by_date_range(date_from, date_to))
+
 def test_search_by_date():
     from datetime import datetime
 
     set_env_variables()
     db_search = DBSearch()
 
-    date_from = datetime.strptime('03/01/2023', '%M/%d/%Y')
+    date_from = datetime.strptime('07/08/2023', '%m/%d/%Y')
     print_results(db_search.search_by_date(date_from))
 
 def test_search_by_location():
@@ -91,4 +107,5 @@ def test_search_by_location():
 
 if __name__ == '__main__':
     #test_search_by_location() 
-    test_search_by_date()
+    #test_search_by_date()
+    test_search_by_date_range()
