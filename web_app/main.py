565f83053ed81d839ab48c86df301f187a423b18
gonandrap <gonzalo.a.r@gmail.com>
2023-07-21 15:44:14 -0700

    submit latest changes

--- web_app/main.py
+++ web_app/main.py
@@ -5,6 +5,7 @@ from business.search_criteria import SearchCriteria
 from model import SearchModel, UfoObservation, UfoObservationList
 from typing import Union
 from database.search import DBSearch
+import os
 
 app = FastAPI()
 search_engine = DBSearch()
@@ -12,10 +13,11 @@ search_engine = DBSearch()
 @app.get('/')
 async def root():
     return {'message':'Welcome to the best UFO DB search engine!'}
+    
 
-@app.post('/search', response_model=UfoObservationList)
+@app.post('/search', response_model=UfoObservationList)              
 async def search(item: SearchModel):
     return SearchCriteria.create(item).search(search_engine)
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8000)
+    uvicorn.run(app, host=os.environ.setdefault('HOSTNAME', 'localhost'), port=os.environ.setdefault('PORT', '8000'))
